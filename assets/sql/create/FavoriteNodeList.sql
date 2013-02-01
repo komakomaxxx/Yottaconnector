b8829d21389bf18bdb98261f53730ae73dc5d99c
g009c1027 <g009c1027@b320fce4-7ebb-3340-a898-b594cac277fd>
2013-02-01 02:22:10 +0000

    git-svn-id: http://172.16.7.72/svn/YottaConnector/Yottaconnecter@325 b320fce4-7ebb-3340-a898-b594cac277fd

---
+++ assets/sql/create/FavoriteNodeList.sql
@@ -0,0 +1,8 @@
+CREATE TABLE FavoriteNodeList
+(
+MACAddr VARCHAR PRIMARY KEY NOT NULL,
+UserName VARCHAR NOT NULL,
+UserIcon BLOB NOT NULL,
+UserProfile TEXT
+)
+;
---
+++ assets/sql/drop/FavoriteNodeList.sql
@@ -0,0 +1,2 @@
+DROP TABLE FavoriteNodeList
+;
