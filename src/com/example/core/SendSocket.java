dd605aa2bd3295ba7d4ba7ebf7ab03b30140129e
komax <komax@b320fce4-7ebb-3340-a898-b594cac277fd>
2013-02-05 02:20:02 +0000

    git-svn-id: http://172.16.7.72/svn/YottaConnector/Yottaconnecter@422 b320fce4-7ebb-3340-a898-b594cac277fd

--- src/com/example/core/SendSocket.java
+++ src/com/example/core/SendSocket.java
@@ -49,8 +49,8 @@ public class SendSocket implements Runnable{
 		this.host = "192.168.0.101";
 	}
 
-	@Overridez
-	public synchronied void run() {
+	@Override
+	public synchronized void run() {
 		// TODO 自動生成されたメソッド・スタブ
 		Socket socket;
 		try {
