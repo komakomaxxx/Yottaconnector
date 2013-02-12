e74eec6c09ba8035040e46fba7434f6f1124dccf
g009c1027 <g009c1027@b320fce4-7ebb-3340-a898-b594cac277fd>
2013-02-12 07:22:06 +0000

    git-svn-id: http://172.16.7.72/svn/YottaConnector/Yottaconnecter@541 b320fce4-7ebb-3340-a898-b594cac277fd

---
+++ src/com/example/yottaconnecter/FileFragment.java
@@ -0,0 +1,40 @@
+package com.example.yottaconnecter;
+
+import com.example.yottaconnecter.*;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Context;
+
+public class FileFragment extends Fragment  {
+	public FileFragment() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public View onCreateView(
+		LayoutInflater inflater, 
+		ViewGroup container, 
+		Bundle savedInstanceState) {
+		
+		return inflater.inflate(R.layout.file_fragment ,container, false);
+	}
+
+	public void onMyListenerEvent() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
